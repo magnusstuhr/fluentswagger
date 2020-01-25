83e8a7e6e9f77185fc198c93e2e5f879387648cd
Magnus Stuhr <magnus.stuhr@gmail.com>
2020-01-25 10:28:45 +0100

    Add SwaggerIntegrationTests with corresponding isolated functionality for verifying that the Swagger endpoint is running as expected.

---
+++ test/FluentSwaggerTests/Swagger/Endpoint/SwaggerEndpointConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace FluentSwaggerTests.Swagger.Endpoint
+{
+    internal class SwaggerEndpointConfig
+    {
+        public SwaggerEndpointInfo Info { get; set; }
+    }
+}
---
+++ test/FluentSwaggerTests/Swagger/SwaggerRuntimeInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FluentSwaggerTests.Swagger
+{
+    public class SwaggerRuntimeInfo : IEquatable<SwaggerRuntimeInfo>
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Version { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as SwaggerRuntimeInfo);
+        }
+
+        public bool Equals(SwaggerRuntimeInfo other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Equals(Title, other.Title) && Equals(Description, other.Description) &&
+                   Equals(Version, other.Version);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Title, Description, Version);
+        }
+
+        private static bool Equals(string s1, string s2)
+        {
+            return string.Equals(s1, s2);
+        }
+    }
+}
